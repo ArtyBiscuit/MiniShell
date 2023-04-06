fc8fd293a6d4ecb57968d511759ee0dc16dcff4c
Arthur Forgeard <arforgea@2C7.42angouleme.fr>
2023-04-06 14:17:28 +0200

    FIX | fix split_pipes in plite.c and Norm OK | #060423 | by: Arty

--- src/split.c
+++ src/split.c
@@ -6,10 +6,34 @@
 /*   By: axcallet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 09:39:12 by axcallet          #+#    #+#             */
-/*   Updated: 2023/04/05 15:21:38 by axcallet         ###   ########.fr       */
+/*   Updated: 2023/04/06 14:17:01 by arforgea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../inc/minishell.h"
+
+static char	*get_str(char *str, int *index, int *j)
+{
+	char	*f_tab;
+
+	f_tab = NULL;
+	while (str[*index])
+	{
+		if (str[*index + *j] == '"')
+			*j += skip_argument(&str[*index + *j]);
+		if (str[*index + *j] == '|' || str[*index + *j] == '\0')
+		{
+			f_tab = malloc(sizeof(char) * (*j + 1));
+			if (!f_tab)
+				return (NULL);
+			ft_memcpy(f_tab, &str[*index], *j);
+			f_tab[*j + *index] = '\0';
+			*index += *j + 1;
+			break ;
+		}
+		(*j)++;
+	}
+	return (f_tab);
+}
 
 char	**split_pipes(char *str)
 {
@@ -24,67 +48,12 @@ char	**split_pipes(char *str)
 	f_tab = malloc(sizeof(char *) * (get_pipes_number(str) + 1));
 	if (!f_tab)
 		return (NULL);
-    //f_tab[get_pipes_number(str)] = NULL;
 	while (i < get_pipes_number(str))
 	{
-		while (str[index])
-		{
-			if (str[index + j] == '"')
-				j += skip_argument(&str[index + j]);
-			if (str[index + j] == '|' || str[index + j] == '\0')
-			{
-				f_tab[i] = malloc(sizeof(char) * (j + 1));
-				if (!f_tab[i])
-					return (NULL);
-				ft_memcpy(f_tab[i], &str[index], j);
-				f_tab[i][j + index] = '\0';
-				index += j + 1;
-				break ;
-			}
-			j++;
-		}
+		f_tab[i] = get_str(str, &index, &j);
 		i++;
 		j = 0;
 	}
 	f_tab[i] = NULL;
 	return (f_tab);
 }
-
-/*
-char	**split_pipes(char *str)
-{
-	int	i;
-	int j;
-	int index;
-	char	**f_tab;
-
-	int yolo = get_pipes_number(str);
-
-	f_tab = malloc(sizeof(char *) * (get_pipes_number(str) + 1));
-	if (!f_tab)
-		return (NULL);
-	f_tab[get_pipes_number(str) + 1] = NULL;
-	i = 0;
-	j = 0;
-	index = 0;
-	while (i < get_pipes_number(str))
-	{
-		while(str[j + index])
-		{
-			if (str[index + j] == '"')
-				j += skip_argument(&str[j]);
-			if (str[index + j] == '|')
-			{
-				f_tab[i] = malloc(sizeof(char) * j);
-				f_tab[i][j - index] = '\0';
-				index = j;
-				j = 0;
-				break ;
-			}
-			j++;
-		}
-		i++;
-	}
-	return (f_tab);
-}
-*/
